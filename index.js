079303e37b87306c8cb7bbacabdf86fc17567a48
RennerMe <rennercasemiro@icloud.com>
2025-11-04 21:44:53 -0300

    package.json  (fix) type - module

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 let api = express();
 
+api.get("/", (req, res) => {
+    res.json( "Pagina inicial");
+})
+
 api.get("/user", (req, res) => {
     res.json( "Renner Medeiros");
 })
@@ -9,3 +13,4 @@ api.get("/user", (req, res) => {
 api.listen(3000, () => {
     console.log('O servidor esta na porta http://localhost:3000');
 });
+ 
